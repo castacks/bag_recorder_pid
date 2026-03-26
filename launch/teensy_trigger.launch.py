dd16cf4b493ca2ffede53e23465950ace38694ae
Parv Maheshwari <parvmaheshwari2002@gmail.com>
2026-03-26 12:07:45 -0400

    tf recording added

--- launch/teensy_trigger.launch.py
+++ launch/teensy_trigger.launch.py
@@ -136,7 +136,7 @@ def generate_launch_description():
         namespace="",
         package="rclcpp_components",
         executable="component_container_isolated",
-        composable_node_descriptions=make_recorder_nodes(keys=[f"trigger"]),
+        composable_node_descriptions=make_recorder_nodes(keys=[f"trigger"]) + make_recorder_nodes(keys=[f"tf"]),
         output="screen",
     )
 
